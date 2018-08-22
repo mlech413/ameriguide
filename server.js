a54e15598993c777aedbb8da750f9a78b9d82b91
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-08-22 14:10:06 -0400

    final commit

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ameriguidelist");
+var MONGODB_URI =mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ameriguidelist");
 
-// Start the API server
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
